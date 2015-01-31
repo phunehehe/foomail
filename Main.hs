d8b712ea689c5fcf096dff72ba55ad23a2aab2b3
Hoang Xuan Phu <phuhimself@phunehehe.net>
2015-01-31 16:11:06 +0800

    get rid of postgres

--- Main.hs
+++ Main.hs
@@ -10,10 +10,6 @@ import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Proxy
 import Data.Text (Text)
-import Database.PostgreSQL.Simple
-import Database.PostgreSQL.Simple.FromRow
-import Database.PostgreSQL.Simple.ToField
-import Database.PostgreSQL.Simple.ToRow
 import GHC.Generics
 import Network.Wai.Handler.Warp (run)
 import Servant
@@ -27,33 +23,25 @@ data Book = Book
 instance FromJSON Book
 instance ToJSON Book
 
--- PostgreSQL instances
-instance FromRow Book where
-  fromRow = Book <$> field <*> field
-
-instance ToRow Book where
-  toRow book = [ toField (title book)
-               , toField (author book)
-               ]
-
              -- we explicitly say we expect a request body,
              -- of type Book
 type BookApi = "books" :> ReqBody Book :> Post Book  -- POST /books
           :<|> "books" :> Get [Book]                 -- GET /books
 
-server :: Connection -> Server BookApi
-server conn = postBook
-         :<|> getBooks
+server :: Server BookApi
+server = postBook
+    :<|> getBooks
 
   where -- the aforementioned 'ReqBody' automatically makes this handler
         -- receive a Book argument
-        postBook book = liftIO $ execute conn "insert into books values (?, ?)" book >> return book
-        getBooks      = liftIO $ query_ conn "select * from books"
+        postBook book = liftIO $ do
+            print "insert into books values (?, ?)"
+            return $ Book "test" "test"
+        getBooks      = liftIO $ print "select * from books" >> return [Book "test" "test"]
 
 bookApi :: Proxy BookApi
 bookApi = Proxy
 
 main :: IO ()
 main = do
-  conn <- connectPostgreSQL "host=localhost user=bookstore dbname=bookstore"
-  run 8080 (serve bookApi $ server conn)
+  run 8080 (serve bookApi $ server)
