a45a6f0f18faa4a29fce62215d0ab7a280d5c9a2
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-05-01 18:59:26 +0800

    try separate jobs

--- test.sh
+++ test.sh
@@ -3,22 +3,36 @@ set -efuxo pipefail
 
 export NIX_PATH=nixpkgs=$HOME/.nix-defexpr/channels/nixos-16.03
 
-# This will become just nodePackages.eslint after the next NixOS release
-NEW_PATH=$(nix-build --no-out-link --expr '
-  let inherit (import <nixpkgs> {}) pkgs;
-  in pkgs.callPackage ./eslint {}
-')/bin
-
+NEW_PATH=
 for p in nix
 do
   NEW_PATH=$(nix-build --no-out-link '<nixpkgs>' --attr $p)/bin:$NEW_PATH
 done
 PATH=$NEW_PATH
 
-# This conveniently has `cabal test` baked in
-nix-build --no-out-link --expr '
-  let inherit (import <nixpkgs> {}) pkgs;
-  in pkgs.callPackage ./package.nix {}
-'
+run_hspec() {
+  # This conveniently has `cabal test` baked in
+  nix-build --no-out-link --expr '
+    let inherit (import <nixpkgs> {}) pkgs;
+    in pkgs.callPackage ./package.nix {}
+  '
+}
 
-eslint static/js/main.js
+run_eslint() {
+
+  # This will become just nodePackages.eslint after the next NixOS release
+  PATH=$(nix-build --no-out-link --expr '
+    let inherit (import <nixpkgs> {}) pkgs;
+    in pkgs.callPackage ./eslint {}
+  ')/bin:$PATH
+
+  eslint static/js/main.js
+}
+
+case ${1:-} in
+  eslint) run_eslint;;
+  hspec) run_hspec;;
+  *)
+    run_hspec
+    run_eslint;;
+esac
