dc99143dc62253934dbc94dfa01da2b7ad67c996
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-05-01 18:48:34 +0800

    also add eslint

--- test.sh
+++ test.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-set -efuo pipefail
+set -efuxo pipefail
+
+export NIX_PATH=nixpkgs=$HOME/.nix-defexpr/channels/nixos-16.03
+
+# This will become just nodePackages.eslint after the next NixOS release
+NEW_PATH=$(nix-build --no-out-link --expr '
+  let inherit (import <nixpkgs> {}) pkgs;
+  in pkgs.callPackage ./eslint {}
+')/bin
+
+for p in nix
+do
+  NEW_PATH=$(nix-build --no-out-link '<nixpkgs>' --attr $p)/bin:$NEW_PATH
+done
+PATH=$NEW_PATH
+
+# This conveniently has `cabal test` baked in
 nix-build --no-out-link --expr '
   let inherit (import <nixpkgs> {}) pkgs;
   in pkgs.callPackage ./package.nix {}
 '
+
+eslint static/js/main.js
