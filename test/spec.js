936f646dbb4bf54bc399475e5a8b23b20bab0052
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-05-01 20:35:46 +0800

    use absolute path in spec.js thanks to http://stackoverflow.com/q/16769057/168034

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,8 @@
+var currentFile = require('fs').absolute(require('system').args[4])
+var index = currentFile + '/../../static/index.html'
+
 casper.test.begin('fooMail', 1, function suite (test) {
-  casper.start('./static/index.html')
+  casper.start(index)
   casper.then(function () {
     test.assertTitle('fooMail', 'Page title is the one expected')
   })
